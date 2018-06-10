00fb31dbb95dc27a9d0bc6861a9e4b408734a87b
elisasaltori <elisa.trujillo@usp.br>
2018-06-10 08:22:00 -0300

    melee combat added

---
+++ treinamento_g3/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform Alvo;
+    public float HP;
+    public float Velocidade;
+    public float Dano;
+    public bool CombateProximo;
+    
+	
+	void Start () {
+       // NavMeshAgent Agente = gameObject.GetComponent<NavMeshAgent>();
+       // Agente.destination = Alvo.position;
+	}
+	
+	
+    public void LevouDano(float DanoRecebido)
+    {
+        HP -= DanoRecebido;
+        if(HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	void Update () {
+		
+	}
+}
