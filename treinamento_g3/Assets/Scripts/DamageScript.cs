fe91fa14dbf1d6ce8f951b8416f042b692bd0cff
elisasaltori <elisa.trujillo@usp.br>
2018-06-13 20:45:05 -0300

    manual merge with Lucas branch

---
+++ treinamento_g3/Assets/Scripts/DamageScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour {
+
+    public float Dano;
+    public float Velocidade;
+    private Rigidbody2D RB;
+
+	void Start () {
+        RB = gameObject.GetComponent<Rigidbody2D>();
+        RB.velocity += new Vector2(-Velocidade, 0);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            collision.GetComponent<EnemyController>().LevouDano(Dano);
+            Destroy(gameObject);
+        }
+    }
+}
