9415916e1348019e342747e4c20a23ada1278f2e
elisasaltori <elisa.trujillo@usp.br>
2018-08-26 17:39:17 -0300

    working on elevator + dialogue panel updated

---
+++ treinamento_g3/Assets/Scripts/ElevatorEnding.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorEnding : MonoBehaviour {
+
+    GameObject boss;
+    Animator doorAnimator;
+    Collider doorCollider;
+    MenuManager menumanager;
+
+
+	// Use this for initialization
+	void Start () {
+        //get boss
+        boss = GameObject.Find("Boss");
+        doorAnimator = GetComponent<Animator>();
+        doorCollider = GetComponent<Collider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//check if boss is dead
+        if(boss == null)
+        {
+            doorAnimator.SetBool("open", true);
+            doorCollider.isTrigger = true;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //close elevator door and wait for animation
+        }
+    }
+}
