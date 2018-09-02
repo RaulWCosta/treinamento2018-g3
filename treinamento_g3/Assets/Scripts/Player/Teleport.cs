47ea7739553114347ddc52b65097adf801308ba0
Luis Felipe Chaves <pi.luis.f3@usp.br>
2018-09-02 10:37:07 -0300

    Simplificação do script de teleport

--- treinamento_g3/Assets/Scripts/Player/Teleport.cs
+++ treinamento_g3/Assets/Scripts/Player/Teleport.cs
@@ -3,65 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour {
-
-    public bool invencible;
-    private float teleportRange;
-    private float timeBetweenDodges;
-    private Vector3 teleport;
-    private bool teleporting;
     private Animator animator;
-    private float realTeleportRange;
+    private Vector3 direction;
+    public bool invencible;
+    public float range;
 
     // Use this for initialization
     void Start () {
         animator = this.GetComponent<Animator>();
-        teleportRange = 3f;
-        timeBetweenDodges = 1f;
+        invencible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //Quando apertar o shift esquerdo, esquiva
-        if (Input.GetKeyDown(KeyCode.LeftShift) && teleporting == false && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && (realTeleportRange = RayCast()) > 1)
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            teleporting = true;                                                                                                           //Esa varíávle impede que o teleporte possa ser feito varias vezes
-            invencible = true;                                                                                                            //Fica invencível
+            direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized * range;
+            invencible = true;
+            Debug.Log("invencible = " + invencible);
             animator.SetTrigger("teleport");
-            Invoke("NotInvencible", timeBetweenDodges / 2);                                                                               //Tempo pra deixar de ficar invencível
-            Invoke("AllowTeleport", timeBetweenDodges);                                                                                   //Tempo pra permitir o teletransporte novamente
         }
-        Debug.DrawRay(this.transform.position, new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")), Color.green);
     }
 
     void Dodge()
     {
-        teleport = Vector3.right * Input.GetAxis("Horizontal");
-        teleport += Vector3.forward * Input.GetAxis("Vertical");
-        teleport.Normalize();
-        teleport *= realTeleportRange;
-        transform.Translate(teleport);
-        animator.SetBool("walk", false);
-    }
-
-    void AllowTeleport()
-    {
-        teleporting = false;
-    }
-    void NotInvencible()
-    {
+        this.transform.Translate(direction, Space.World);
         invencible = false;
+        Debug.Log("invencible = " + invencible);
     }
 
     float RayCast()
     {
-        RaycastHit ray;
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-        if (Physics.Raycast(this.transform.position, direction, out ray, teleportRange))
-        {
-            Debug.Log(ray.distance);
-            return ray.distance;
-        }
-        Debug.Log("No ray 3");
-            return 3f;
+        return 0f;
     }
 }
